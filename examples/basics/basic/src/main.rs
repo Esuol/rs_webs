d52bbfb7f45bfb2970e97e916b828569daa7599d
berlinen <1292393108@qq.com>
2024-04-29 13:41:55 +0800

    feat: init basic

---
+++ examples/basics/basic/src/main.rs
@@ -0,0 +1,17 @@
+use std::{convert::Infallible, io};
+
+use actix_files::{Files, NamedFile};
+use actix_session::{storage::CookieSessionStore, Session, SessionMiddleware};
+use actix_web::{
+    error, get,
+    http::{
+        header::{self, ContentType},
+        Method, StatusCode,
+    },
+    middleware, web, App, Either, HttpRequest, HttpResponse, HttpServer, Responder, Result,
+};
+use async_stream::stream;
+
+fn main() {
+    println!("Hello, world!");
+}
