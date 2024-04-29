34f492b1a60c2c18a5ba4d17367447d93801f06e
berlinen <1292393108@qq.com>
2024-04-29 15:12:16 +0800

    feat: custom error

--- examples/basics/error-handling/src/main.rs
+++ examples/basics/error-handling/src/main.rs
@@ -1,3 +1,33 @@
+use actix_web::{web, App, Error, HttpResponse, HttpServer, ResponseError};
+use derive_more::Display;
+use rand::{
+    distributions::{Distribution, Standard},
+    thread_rng, Rng,
+};
+
+#[derive(Debug, Display)]
+pub enum CustomError {
+    #[display(fmt = "Custom Error 1")]
+    CustomOne,
+    #[display(fmt = "Custom Error 2")]
+    CustomTwo,
+    #[display(fmt = "Custom Error 3")]
+    CustomThree,
+    #[display(fmt = "Custom Error 4")]
+    CustomFour,
+}
+
+impl Distribution<CustomError> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> CustomError {
+        match rng.gen_range(0..4) {
+            0 => CustomError::CustomOne,
+            1 => CustomError::CustomTwo,
+            2 => CustomError::CustomThree,
+            _ => CustomError::CustomFour,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
