9c25e17aee0d34772cfd19b01bbbcfbbad40b457
berlinen <1292393108@qq.com>
2024-04-29 13:54:15 +0800

    feat: basic welcome

--- examples/basics/basic/src/main.rs
+++ examples/basics/basic/src/main.rs
@@ -19,6 +19,25 @@ async fn favicon() -> Result<impl Responder> {
     Ok(NamedFile::open("static/favicon")?)
 }
 
+#[get("/welcome")]
+async fn welcome(req: HttpRequest, session: Session) -> Result<HttpResponse> {
+    println!("{req:?}");
+
+    // session
+    let mut counter = 1;
+    if let Some(count) = session.get::<i32>("counter")? {
+        println!("SESSION value: {count}");
+        counter = count + 1;
+    }
+    // set counter to session
+    session.insert("counter", counter)?;
+
+    // response
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type(ContentType::plaintext())
+        .body(include_str!("../static/welcome.html")))
+}
+
 fn main() {
     println!("Hello, world!");
 }
