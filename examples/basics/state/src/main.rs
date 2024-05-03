2d3d86fd5dac828db14522bb17a07d5f4a1d5849
berlinen <1292393108@qq.com>
2024-05-03 20:57:42 +0800

    feat: basic state update

--- examples/basics/state/src/main.rs
+++ examples/basics/state/src/main.rs
@@ -13,6 +13,15 @@ use actix_web::{
     App, HttpRequest, HttpResponse, HttpServer,
 };
 
+async fn index(
+    counter_mutex: Data<Mutex<usize>>,
+    counter_cell: Data<Cell<u32>>,
+    counter_atomic: Data<AtomicUsize>,
+    req: HttpRequest,
+) -> HttpResponse {
+    println!("{req:?}");
+}
+
 fn main() {
     println!("Hello, world!");
 }
