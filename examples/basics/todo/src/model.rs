e2ffcf286b459eab9cdd44391f2dd2dfdec5f2a9
berlinen <1292393108@qq.com>
2024-05-06 10:40:54 +0800

    feat: init todo task mode

--- examples/basics/todo/src/model.rs
+++ examples/basics/todo/src/model.rs
@@ -6,9 +6,68 @@ pub struct NewTask {
     pub description: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     pub id: i64,
     pub description: String,
     pub completed: bool,
+}
+
+impl Task {
+    pub async fn all(connection: &SqlitePool) -> Result<Vec<Task>, sqlx::Error> {
+        let tasks = sqlx::query_as!(
+            Task,
+            r#"
+            SELECT *
+            FROM tasks
+            "#
+        )
+        .fetch_all(connection)
+        .await?;
+
+        Ok(tasks)
+    }
+
+    pub async fn insert(todo: NewTask, connection: &SqlitePool) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            r#"
+            INSERT INTO tasks (description)
+            VALUES ($1)
+            "#,
+            todo.description,
+        )
+        .execute(connection)
+        .await?;
+
+        Ok(())
+    }
+
+    pub async fn toogle_with_id(id: i32, connection: &SqlitePool) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            r#"
+            UPDATE tasks
+            SET completed = NOT completed
+            WHERE id = $1
+            "#,
+            id
+        )
+        .execute(connection)
+        .await?;
+
+        Ok(())
+    }
+
+    pub async fn delete_with_id(id: i32, connection: &SqlitePool) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            r#"
+            DELETE FROM tasks
+            WHERE id = $1
+            "#,
+            id
+        )
+        .execute(connection)
+        .await?;
+
+        Ok(())
+    }
 }
