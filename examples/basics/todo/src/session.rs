d6f40f32e6d252c6286c3487cbe5c0cc292aa789
berlinen <1292393108@qq.com>
2024-05-06 11:03:55 +0800

    feat: init todo task session FlashMessage

--- examples/basics/todo/src/session.rs
+++ examples/basics/todo/src/session.rs
@@ -15,3 +15,25 @@ pub fn get_flash(session: &Session) -> Result<Option<FlashMessage>> {
 pub fn clear_flash(session: &Session) {
     session.remove(FLASH_KEY);
 }
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct FlashMessage {
+    pub kind: String,
+    pub message: String,
+}
+
+impl FlashMessage {
+    pub fn success(message: &str) -> Self {
+        Self {
+            kind: "success".to_owned(),
+            message: message.to_owned(),
+        }
+    }
+
+    pub fn error(message: &str) -> Self {
+        Self {
+            kind: "error".to_owned(),
+            message: message.to_owned(),
+        }
+    }
+}
