b73336628b8f6774bfba46f30504165eabf3da71
berlinen <1292393108@qq.com>
2024-05-07 10:09:30 +0800

    feat: todo api mod

--- examples/basics/todo/src/api.rs
+++ examples/basics/todo/src/api.rs
@@ -8,3 +8,31 @@ use serde::Deserialize;
 use sqlx::SqlitePool;
 use tera::{Context, Tera};
 
+use crate::{
+    db,
+    session::{self, FlashMessage},
+};
+
+pub async fn index(
+    pool: web::Data<SqlitePool>,
+    tmpl: web::Data<Tera>,
+    session: Session,
+) -> Result<HttpResponse, Error> {
+    let tasks = db::get_all_tasks(&pool)
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+
+    let mut context = Context::new();
+    context.insert("tasks", &tasks);
+
+    if let Some(flash) = session::get_flash(&session)? {
+        context.insert("msg", &(flash.kind, flash.message));
+        session::clear_flash(&session);
+    }
+
+    let rendered = tmpl
+        .render("index.html.tera", &context)
+        .map_err(error::ErrorInternalServerError)?;
+
+    Ok(HttpResponse::Ok().body(rendered))
+}
--- examples/basics/todo/src/main.rs
+++ examples/basics/todo/src/main.rs
@@ -1,6 +1,7 @@
 mod api;
 mod db;
 mod model;
+mod session;
 
 fn main() {
     println!("Hello, world!");
