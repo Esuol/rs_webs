239323e88a46ad5cd1f4f4b232fed5d5b9257262
berlinen <1292393108@qq.com>
2024-05-07 16:26:32 +0800

    feat: todo run

--- examples/basics/todo/src/main.rs
+++ examples/basics/todo/src/main.rs
@@ -1,7 +1,21 @@
+use std::{env, io};
+
+use actix_files::Files;
+use actix_session::{storage::CookieSessionStore, SessionMiddleware};
+use actix_web::{
+    http,
+    middleware::{ErrorHandlers, Logger},
+    web, App, HttpServer,
+};
+use dotenvy::dotenv;
+use tera::Tera;
+
 mod api;
 mod db;
 mod model;
 mod session;
+
+static SESSION_SIGNING_KEY: &[u8] = &[0; 64];
 
 fn main() {
     println!("Hello, world!");
