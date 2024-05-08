c98dce83717ecfa5238566c5dba0f529744d8f60
berlinen <1292393108@qq.com>
2024-05-08 10:44:53 +0800

    feat: init data factory

--- examples/data-factory/src/main.rs
+++ examples/data-factory/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("starting HTTP server at http://localhost:8080");
 }
