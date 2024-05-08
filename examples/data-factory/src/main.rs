e7868a8f28b3adcd1c42f8736e1b8ee898da1b01
berlinen <1292393108@qq.com>
2024-05-08 10:51:06 +0800

    feat: init data factory run

--- examples/data-factory/src/main.rs
+++ examples/data-factory/src/main.rs
@@ -5,4 +5,20 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     log::info!("starting HTTP server at http://localhost:8080");
+
+    HttpServer::new(move || {
+        App::new()
+            .data_factory(|| async { Ok::<_, ()>(123_usize) })
+            .route(
+                "/",
+                web::to(|data: web::Data<usize>| async move {
+                    assert_eq!(**data, 123);
+                    HttpResponse::NoContent().finish()
+                }),
+            )
+    })
+    .workers(2)
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
