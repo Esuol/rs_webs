cf09d922d32c9883aea844c1f10cf59a54fca20c
berlinen <1292393108@qq.com>
2024-05-09 10:49:30 +0800

    feat: diesel find_user_by_uid

--- examples/databases/diesel/src/actions.rs
+++ examples/databases/diesel/src/actions.rs
@@ -1,6 +1,20 @@
 use diesel::prelude::*;
-use uuid::uuid;
+use uuid::Uuid;
 
 use crate::models;
 
 type DbError = Box<dyn std::error::Error + Send + Sync>;
+
+pub fn find_user_by_uid(
+    conn: &mut SqliteConnection,
+    uid: Uuid,
+) -> Result<Option<models::User>, DbError> {
+    use crate::schema::users::dsl::*;
+
+    let user = users
+        .filter(id.eq(uid.to_string()))
+        .first::<models::User>(conn)
+        .optional()?;
+
+    Ok(user)
+}
